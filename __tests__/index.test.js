fcfc367164082fa2ce292dc2ff4d8dff6446cc9d
Nikita Naumenko <naumenkoniki@gmail.com>
2020-09-28 09:51:39 +0300

    Fix linter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,21 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
+import path, { dirname } from 'path';
+
 import fs from 'fs';
 import genDiff from '../index';
 
+/* eslint-disable no-underscore-dangle */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+/* eslint-enable no-underscore-dangle */
 
 test('generate flat json diff', () => {
-  const filepath1 = path.resolve(__dirname, '..', '__fixtures__', 'first_file.json')
-  const filepath2 = path.resolve(__dirname, '..', '__fixtures__', 'second_file.json')
-  const resultPath = path.resolve(__dirname, '..', '__fixtures__', 'result')
+  const filepath1 = path.resolve(__dirname, '..', '__fixtures__', 'first_file.json');
+  const filepath2 = path.resolve(__dirname, '..', '__fixtures__', 'second_file.json');
+  const resultPath = path.resolve(__dirname, '..', '__fixtures__', 'result');
 
   const result = fs.readFileSync(resultPath, 'utf8');
 
-  expect(genDiff(filepath1, filepath2)).toEqual(result)
-})
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+});
